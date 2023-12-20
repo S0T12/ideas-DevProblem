2fa0875232345c90e6d8cfe662d889e2e1ac078b
s0a1e2 <oops2198@gmail.com>
2023-12-20 09:47:52 +0330

    config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { IdeasController } from './ideas.controller';
 import { IdeasService } from './ideas.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Idea, IdeaSchema } from './schemas/idea.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/ideas-devProblem'),
+    MongooseModule.forFeature([{ name: Idea.name, schema: IdeaSchema }]),
+  ],
   controllers: [IdeasController],
   providers: [IdeasService],
 })
