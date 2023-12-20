2ef90210cc9dcf0f30deec4ec9e08b2137ddf4e3
s0a1e2 <oops2198@gmail.com>
2023-12-20 09:48:29 +0330

    update DTO's

--- src/dto/create-idea.dto.ts
+++ src/dto/create-idea.dto.ts
@@ -1,1 +1,16 @@
-export class CreateIdeaDto {}
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateIdeaDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  description: string;
+
+  @IsString()
+  creator: string;
+
+  @IsOptional()
+  @IsNumber()
+  problem: number;
+}
--- src/dto/update-idea.dto.ts
+++ src/dto/update-idea.dto.ts
@@ -1,6 +1,10 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PickType } from '@nestjs/mapped-types';
 import { CreateIdeaDto } from './create-idea.dto';
+import { ObjectId } from 'mongodb';
 
-export class UpdateIdeaDto extends PartialType(CreateIdeaDto) {
-  id: number;
+export class UpdateIdeaDto extends PickType(CreateIdeaDto, [
+  'title',
+  'description' as const,
+]) {
+  id: ObjectId;
 }
