9543981c6c0b0cee63574349fc921e984ce96ec1
s0a1e2 <oops2198@gmail.com>
2024-01-07 02:11:08 +0330

    validation user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { IdeasController } from './ideas.controller';
 import { IdeasService } from './ideas.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Idea, IdeaSchema } from './schemas/idea.schema';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
+    ClientsModule.register([
+      {
+        name: 'USERS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'users_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
     MongooseModule.forRoot('mongodb://localhost:27017/ideas-devProblem'),
     MongooseModule.forFeature([{ name: Idea.name, schema: IdeaSchema }]),
   ],
--- src/ideas.service.ts
+++ src/ideas.service.ts
@@ -1,16 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { CreateIdeaDto } from './dto/create-idea.dto';
 import { UpdateIdeaDto } from './dto/update-idea.dto';
 import { ObjectId } from 'mongodb';
 import { InjectModel } from '@nestjs/mongoose';
 import { Idea } from './schemas/idea.schema';
 import { Model } from 'mongoose';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class IdeasService {
-  constructor(@InjectModel(Idea.name) private ideaModel: Model<Idea>) {}
+  constructor(
+    @InjectModel(Idea.name) private ideaModel: Model<Idea>,
+    @Inject('USERS_SERVICE') private readonly client: ClientProxy,
+  ) {}
   async create(createIdeaDto: CreateIdeaDto) {
     try {
+      const user = await firstValueFrom(
+        this.client.send<string>('findByUsername', createIdeaDto.creator),
+      );
+      if (!user) throw new BadRequestException('User not exists');
+
       const idea = new this.ideaModel(createIdeaDto);
       return await idea.save();
     } catch (error) {
